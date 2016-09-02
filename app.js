2d0d67f711be97ca2ffa6ca2f38455735c7e044c
Brad <brthiess@ualberta.ca>
2016-09-01 18:31:24 -0600

    'asdf'

---
+++ app.js
@@ -0,0 +1,19 @@
+/*
+ * Module dependencies
+ */
+var express = require('express');
+
+
+var app = express();
+
+app.set('views', __dirname + '/views')
+app.set('view engine', 'pug')
+app.use(express.logger('dev'))
+
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', function (req, res) {
+  res.render('index'
+  )
+})
+app.listen(3000)
