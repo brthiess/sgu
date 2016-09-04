f7f0ee3091d65ac94280e8409ad7e313bdbdc1b5
Brad <brthiess@ualberta.ca>
2016-09-03 23:57:00 -0600

    'asdf'

--- app.js
+++ app.js
@@ -16,4 +16,8 @@ app.get('/', function (req, res) {
   res.render('index'
   )
 })
+app.get('/chart', function (req, res) {
+  res.render('chart'
+  )
+})
 app.listen(3000)
--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,140 @@
-console.log("hell");
+function loadGraph(graph, graphType, page, chartContainer){ 	
+	transitionToGraph();
+	history.pushState(null, page, page);
+	if(graphType = 'barchart'){
+		loadBarChart(graph, chartContainer);
+	}
+}
+
+function transitionToGraph(){
+	document.getElementsByTagName("body")[0].style.background = "white";
+	document.getElementById("main-page").style.opacity = 0;
+	document.getElementById("graph-page").style.opacity = 1;
+	document.getElementById("graph-page").style.display = 'block';
+}
+
+function loadBarChart(file, chartContainer){
+
+	var margin = {top: 80, right: 180, bottom: 80, left: 180},
+		width = 960 - margin.left - margin.right,
+		height = 500 - margin.top - margin.bottom;
+
+	var svg = d3.select(chartContainer).append("svg")
+		.attr("width", width + margin.left + margin.right)
+		.attr("height", height + margin.top + margin.bottom)
+		.append("g")
+		.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+	d3.tsv(file, function(error, data){
+
+		// filter year
+		var data = data.filter(function(d){return true;});
+		// Get every column value
+		var elements = Object.keys(data[0])
+			.filter(function(d){
+				return ((d != "Name"));
+			});
+		var selection = elements[0];
+
+		var y = d3.scale.linear()
+				.domain([0, d3.max(data, function(d){
+					return +d[selection];
+				})])
+				.range([height, 0]);
+
+		var x = d3.scale.ordinal()
+				.domain(data.map(function(d){ return d.Name;}))
+				.rangeBands([0, width]);
+
+
+		var xAxis = d3.svg.axis()
+			.scale(x)
+			.orient("bottom");
+
+		var yAxis = d3.svg.axis()
+			.scale(y)
+			.orient("left");
+
+		svg.append("g")
+			.attr("class", "x axis")
+			.attr("transform", "translate(0," + height + ")")
+			.call(xAxis)
+			.selectAll("text")
+			.style("font-size", "8px")
+			.style("text-anchor", "end")
+			.attr("dx", "-.8em")
+			.attr("dy", "-.55em")
+			.attr("transform", "rotate(-90)" );
+
+
+		svg.append("g")
+			.attr("class", "y axis")
+			.call(yAxis);
+
+		svg.selectAll("rectangle")
+			.data(data)
+			.enter()
+			.append("rect")
+			.attr("class","rectangle")
+			.attr("width", width/data.length)
+			.attr("height", function(d){
+				return height - y(+d[selection]);
+			})
+			.attr("x", function(d, i){
+				return (width / data.length) * i ;
+			})
+			.attr("y", function(d){
+				return y(+d[selection]);
+			})
+			.append("title")
+			.text(function(d){
+				return d.Name + " : " + d[selection];
+			});
+		/*
+		var selector = d3.select("#drop")
+			.append("select")
+			.attr("id","dropdown")
+			.on("change", function(d){
+				selection = document.getElementById("dropdown");
+
+				y.domain([0, d3.max(data, function(d){
+					return +d[selection.value];})]);
+
+				yAxis.scale(y);
+
+				d3.selectAll(".rectangle")
+					.transition()
+					.attr("height", function(d){
+						return height - y(+d[selection.value]);
+					})
+					.attr("x", function(d, i){
+						return (width / data.length) * i ;
+					})
+					.attr("y", function(d){
+						return y(+d[selection.value]);
+					})
+					.ease("linear")
+					.select("title")
+					.text(function(d){
+						return d.Name + " : " + d[selection.value];
+					});
+		  
+				d3.selectAll("g.y.axis")
+					.transition()
+					.call(yAxis);
+
+			 });
+
+		selector.selectAll("option")
+		  .data(elements)
+		  .enter().append("option")
+		  .attr("value", function(d){
+			return d;
+		  })
+		  .text(function(d){
+			return d;
+		  }) */
+
+
+	});
+}
